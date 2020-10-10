9831d43d6423005cb60325d406e51b46a4e180ed
Patrick <wangzusong@juwang.cn>
2020-10-10 19:18:48 +0800

    'deploy-gh'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,15 +10,14 @@ module.exports = {
       lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
-       
       sidebar: 'auto',
       sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-      lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+      lastUpdated: '最后更新时间', // 文档更新时间：每个文件git最后提交的时间
       
      //导航栏配置
       nav: [
           { text: '首页', link: '/'},
-          { text: 'Android', link: '/android/' },
+          { text: 'Android', link: '/android/'},
           {
             text: '博客',
              
@@ -33,7 +32,6 @@ module.exports = {
                 { text: 'others', link: '/index/others/others.md' },
             ]
           },
-           
           {
               text: '链接',
               //ariaLabel: 'Language Menu',
@@ -42,91 +40,13 @@ module.exports = {
                 { text: '博客园', link: 'https://www.cnblogs.com/chandlerwong/' },
               ]
             },
-             
-            {text:'关于', link:'/guide/guide.md'},
+            {text:'关于', link:'/abut.md'},
       ],
       displayAllHeaders: true, // 默认值：false
       activeHeaderLinks: false, // 默认值：true
-       
       //侧边栏配置
       sidebar: {
-           '/index/': [
-              // 侧边栏在 /index/ 目录上
-              '/index/',
-              {
-                title: 'Html',
-                collapsable: false, // 不可折叠
-                children: [
-                  '/html/one.md',
-              ]
-              },
-              {
-                title: 'CSS',
-                collapsable: false, // 不可折叠
-                children: [
-                    '/index/css/one.md',
-                     
-              ]
-              },
-              // 侧边栏在 /javascript/ 目录上
-              {
-                title: 'Javascript',
-                collapsable: true, // 不可折叠
-                children: [
-                  '/index/javascript/javascript.md'
-                ]
-              },
-              // 侧边栏在 /node.js/ 目录上
-              {
-                  title: 'nodejs',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/index/nodejs/nodejs.md',
-                    
-                  ]
-                },
-                // 侧边栏在 /react.js/ 目录上
-              {
-                  title: 'react',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/index/nodejs/react.md',
-                     
-                  ]
-                },
-                // 侧边栏在 /others/ 目录上
-                {
-                  title: '其它',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/index/others/others.md',
-                     
-                  ]
-                },
-                // 侧边栏在 /vue.js/ 目录上
-              {
-                  title: 'vuejs',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/index/vue/vue.md',
-                     
-                  ]
-                },
-                   // 侧边栏在 /vx/ 目录上
-              {
-                  title: 'vx',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/index/vx/vx.md',
-                     
-                  ]
-                },
-          ]
-        }
-          },
-           
-          algolia: {
-              apiKey: '<API_KEY>',
-              indexName: '<INDEX_NAME>'
-            }
+        "/android/ndk/": "auto"
       }
+     },
+}
